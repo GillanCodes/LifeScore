43f1484aec453cc1149a1982404b0597624fc8dd
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-24 12:46:13 +0200

    edit action done

--- src/controllers/actions.controller.ts
+++ src/controllers/actions.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import log from "../../log";
 import actionModel from "../../models/action.model";
+import { isValidObjectId } from "mongoose";
 
 export const createAction = (req:Request, res:Response) => {
     try {
@@ -21,7 +22,29 @@ export const createAction = (req:Request, res:Response) => {
 }
 
 export const editAction = (req:Request, res:Response) => {
-    
+    try {
+        
+        const {id} = req.params;
+        if (!isValidObjectId(id)) log("id not valid updateAction", "debug");
+        const {title, description, type, color, recurrence, time, counter} = req.body
+        actionModel.findByIdAndUpdate(id, {
+            $set: {
+                title,
+                description,
+                type,
+                color,
+                recurrence,
+                time,
+                counter,
+            }
+        }, {new: true, upsert: true}).then((data) => {
+            return res.status(201).send(data);
+        }).catch((error) => {
+            log (error, "error");
+        })
+    } catch (error:any) {
+       log(error, "error"); 
+    }
 }
 
 export const deleteAction = async (req:Request, res:Response) => {
