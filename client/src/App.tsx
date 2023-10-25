5ede2f1528d7522d0ea9df9cc871fc3bf65245b2
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-25 12:21:16 +0200

    Auth store done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import Routes from "./Routes";
 import './Styles/index.scss';
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { getUser } from "./actions/user.actions";
 
 function App() {
+
+  const [UId, setUId] = useState(null);
+  const dispatch:any = useDispatch();
+
+  useEffect(() => {
+    const fetchToken = async () => {
+        await axios({
+          method:"GET",
+          withCredentials: true,
+          url: `${process.env.REACT_APP_API_URL}/jwtid`
+        }).then((res) => {
+          setUId(res.data);
+        })
+    }
+    fetchToken();
+    if (UId)
+    {
+      dispatch(getUser(UId));
+    }
+  }, [UId, dispatch]);
+
   return (
     <div className="App">
       <Routes />
