f69b9541adb7c7b641842486b1af5df3ae844452
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-25 16:16:37 +0200

    Update Home.tsx

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { isEmpty } from '../Utils';
+import Auth from './Auth/Auth';
+import AppContainer from './App/AppContainer';
+
 
 export default function Home() {
+
+  const userData = useSelector((state:any) => state.userReducer);
+
   return (
-    <div>Home</div>
+    <>
+      {!isEmpty(userData) ? (
+        <AppContainer />
+      ) : (
+        <Auth />
+      )}
+    </>
   )
 }
