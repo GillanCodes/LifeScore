ac8195900f8fa18bf60c9900a2697f88becd278a
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-26 11:02:30 +0200

    Timer module work

---
+++ client/src/Components/App/Modules/Timer.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { IAction } from '../../../types';
+import { isEmpty } from '../../../Utils';
+
+export default function Timer({action} : {action: IAction}) {
+
+    var startDate:number = new Date('Oct 26, 2023 11:00:00').getTime();
+
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        if (load){
+            var countDown = setInterval(() => {
+                var now:number = new Date().getTime();
+                var distance:number = now - startDate;
+
+                var days:number = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours:number = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes:number = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds:number = Math.floor((distance % (1000 * 60)) / 1000);
+
+                const el = document.getElementById('timer') as HTMLElement;
+                el.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            });
+        }
+
+    }, [load]);
+
+    useEffect(() => {
+        if(!isEmpty(action))
+            setLoad(true);
+    });
+
+    return (
+        <div className="timer">
+            <p id="timer"></p>
+        </div>
+    )
+}
