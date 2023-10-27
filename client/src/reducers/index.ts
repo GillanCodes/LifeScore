477d6ea62d935dd662e05becb7d0be4191c6e92a
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-27 10:08:13 +0200

    Update index.ts

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
+import actionsReducer from "./actionReducer";
 
 export default combineReducers({
-    userReducer    
+    userReducer,
+    actionsReducer, 
 });
