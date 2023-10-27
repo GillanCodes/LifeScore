641a5a6c8de0aff76639bea2acf107fed8c97845
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-27 10:08:06 +0200

    added action to combine reducer

--- client/src/reducers/actionReducer.ts
+++ client/src/reducers/actionReducer.ts
@@ -2,7 +2,7 @@ import { GET_ACTIONS } from "../actions/action.actions";
 
 const initialState:object = {};
 
-export default function actionReducer(state = initialState, action:any)
+export default function actionsReducer(state = initialState, action:any)
 {
     switch(action.type)
     {
