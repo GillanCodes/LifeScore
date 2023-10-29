7510b23a5e136e8ed25e6c2c568fbd5e46e44e1d
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2023-10-29 11:54:42 +0100

    Stylized auth page

--- client/src/Components/Auth/Auth.tsx
+++ client/src/Components/Auth/Auth.tsx
@@ -8,13 +8,15 @@ export default function Auth() {
 
     return (
         <div className="auth-box-container">
-            <div className="tabs">
-                <p className={tab === 0 ? 'tab active' : 'tab'} onClick={() => setTab(0)}>Login</p>
-                <p className={tab === 1 ? 'tab active' : 'tab'} onClick={() => setTab(1)}>Register</p>
-            </div>
-            <div>
-                {tab === 0 && (<Login />)}            
-                {tab === 1 && (<Register />)}            
+            <div className="app-container">
+                <div className="tabs">
+                    <p className={tab === 0 ? 'tab active select-none' : 'tab select-none'} onClick={() => setTab(0)}>Login</p>
+                    <p className={tab === 1 ? 'tab active select-none' : 'tab select-none'} onClick={() => setTab(1)}>Register</p>
+                </div>
+                <>
+                    {tab === 0 && (<Login />)}            
+                    {tab === 1 && (<Register />)}            
+                </>
             </div>
         </div>
 
